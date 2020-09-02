a879f35515be770894648e93ab1114709706714d
OHLado <69776512+OHLado@users.noreply.github.com>
2020-09-01 20:38:04 -0400

    Add files via upload

---
+++ JavaScript Data Structures Assignment/Part1.js
@@ -0,0 +1,8 @@
+const array1 = [1, 2, 3, 4, 5];
+
+const copyofArray1 = array1.slice();
+
+console.log(array1);
+console.log(copyofArray1);
+
+var numbers = array1.join();
---
+++ JavaScript Data Structures Assignment/Part2.js
@@ -0,0 +1,4 @@
+myHouse = ["Apartment, Ranch, Flat, Mansion"];
+console.log(myHouse.toString());
+console.log(myHouse.join());
+console.log(myHouse.join('+'));
